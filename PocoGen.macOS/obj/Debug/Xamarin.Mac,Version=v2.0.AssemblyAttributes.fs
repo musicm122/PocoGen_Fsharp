f493150e4cabc6b29b23a0779b10b159a396f9a8
Terrance Smith <terrance@hackerferret.com>
2020-05-28 11:28:15 -0400

    fix gitignore

--- PocoGen.macOS/obj/Debug/Xamarin.Mac,Version=v2.0.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute("Xamarin.Mac,Version=v2.0", FrameworkDisplayName="Xamarin.Mac")>]
-                do ()
