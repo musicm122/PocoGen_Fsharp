c9a5ef51ecec6d3da599c986ca63fa45e9b6f753
Terrance Smith <terrance@hackerferret.com>
2020-06-10 15:53:41 -0400

    Add Test proj

---
+++ PocoGen.Test/ConnectionPageTests.fs
@@ -0,0 +1,23 @@
+﻿module ConnectionPageTests
+
+open System
+open Xunit
+open FsUnit
+open FsUnit.Xunit
+open PocoGen
+open PocoGen.App
+open Fabulous
+open Models
+
+let emptyConnectionStringItem =
+    { ConnectionStringItem.Id = 0
+      ConnectionStringItem.Value = String.Empty
+      ConnectionStringItem.Name = String.Empty }
+
+[<Fact>]
+let ``Init should return a valid initial state``() =
+    let initState =
+        { ConnectionPage.Model.ConnectionString = emptyConnectionStringItem
+          ConnectionPage.Model.CurrentFormState = ConnectionPage.FormState.MissingConnStrValue
+          ConnectionPage.Model.Output = String.Empty }
+    ConnectionPage.init() |> should equal initState
