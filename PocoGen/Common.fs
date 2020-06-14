4c825ccb5f7e89889bdede273a72aafbf88cbf4e
Terrance  L Smith <terrance@hackerferret.com>
2020-06-14 13:51:34 -0400

    Remove unused common methods

--- PocoGen/Common.fs
+++ PocoGen/Common.fs
@@ -7,24 +7,16 @@ open System.IO
 open PocoGen.DomainModels
 
 module Common =
-    ///Returns the case name of the object with union type 'ty.
-    let GetUnionCaseName(x : 'a) =
-        match FSharpValue.GetUnionFields(x, typeof<'a>) with
-        | case, _ -> case.Name
-
-    ///Returns the case names of union type 'ty.
-    let GetUnionCaseNames<'ty>() = FSharpType.GetUnionCases(typeof<'ty>) |> Array.map (fun info -> info.Name)
-
     [<Extension>]
     type StringExtensions =
 
         [<Extension>]
-        static member inline IsNullOrEmpty(_str : string) = String.IsNullOrEmpty(_str)
+        static member inline IsNullOrEmpty(_str: string) = String.IsNullOrEmpty(_str)
 
         [<Extension>]
-        static member inline IsNullOrWhiteSpace(_str : string) = String.IsNullOrWhiteSpace(_str)
+        static member inline IsNullOrWhiteSpace(_str: string) = String.IsNullOrWhiteSpace(_str)
 
-    let IsValidPath(path : FileOutputPath) =
+    let IsValidPath (path: FileOutputPath) =
         try
             let dir = DirectoryInfo(path.FilePath)
             dir.Exists
